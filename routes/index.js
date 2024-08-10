e430334358ab18dcb07e3ca73fd2cbed9c56fc81
RaulPabianCruz <ito.pabian@gmail.com>
2024-08-10 16:32:02 -0700

    Implement message controller in the routes

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,7 @@
 const { Router } = require('express');
+const mssgController = require('../controllers/messageController');
 const router = Router();
 
-const messages = [
-    {
-        text: 'Hi there!',
-        user: 'Amando',
-        added: new Date()
-    },
-    {
-        text: 'Hello World!',
-        user: 'Charles',
-        added: new Date()
-    }
-];
-
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Mini Message Board', messages: messages });
-});
+router.get('/', mssgController.getMessages);
 
 module.exports = router;
--- routes/new.js
+++ routes/new.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
+const mssgController = require('../controllers/messageController')
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('New route');
+    res.render('form', {});
 });
+
+router.post('/', mssgController.postNewMssg);
 
 module.exports = router;
