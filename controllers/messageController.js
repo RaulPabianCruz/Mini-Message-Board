bb687cc043c3e20b5278c462d18a08e41226b23e
RaulPabianCruz <ito.pabian@gmail.com>
2024-08-20 16:35:50 -0700

    Add validation and DB queries to the controller
    Adds data validation to the message form inputs and updates all
    controller middleware functions to use database queries to update or
    display message data.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,28 +1,48 @@
-let messages = [
-    {
-        text: 'Hi there!',
-        user: 'Amando',
-        added: new Date()
-    },
-    {
-        text: 'Hello World!',
-        user: 'Charles',
-        added: new Date()
-    }
+const { body, validationResult } = require('express-validator');
+const db = require('../db/queries');
+const asyncHandler = require('express-async-handler');
+
+const alphaErr = 'must only contain letters';
+const nameLengthErr = 'must be between 1 and 25 characters';
+const mssgLengthErr = 'must be between 1 and 150 characters';
+
+const validateMessage = [
+    body('name').trim()
+    .isAlpha().withMessage('Name ' + alphaErr)
+    .isLength({ min: 1, max: 25 }).withMessage('Name ' + nameLengthErr),
+    body('msg').trim()
+    .isLength({ min: 1, max: 150 }).withMessage('Message ' + mssgLengthErr)
+]
+
+const postNewMssg = [
+    validateMessage,
+    asyncHandler( async (req, res) => {
+        const msgObj = {msg: req.body.msg, name: req.body.name, dateAdded: new Date()};
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).render('form', {
+                errors: errors.array()
+            });
+        }
+        await db.insertMessage(msgObj);
+        res.redirect('/');
+    })
 ];
 
-const postNewMssg = (req, res) => {
-    messages.push({text: req.body.text, user: req.body.user, added: new Date()});
-    res.redirect('/');
-};
-
-const getMessages = (req, res) => {
+const getMessages = asyncHandler( async (req, res) => {
+    const messages = await db.selectAllMessages();
     res.render('index', { title: 'Mini Message Board', messages: messages });
-}
+});
 
-const getMessageById = (req, res) => {
-    const mssgIndex = req.params.mssgId;
-    res.render('message', { message: messages[mssgIndex] });
-}
+const getMessageById = asyncHandler( async (req, res) => {
+    const messages = await db.selectMessageById(req.params.mssgId);
+    if(messages.length == 0){
+        return res.render('message', { 
+            message: { msg: '', name: '', dateAdded: '' },
+            errors: [{ msg: 'Message not found'}]
+        });
+    }
+    res.render('message', { message: messages[0] });
+});
 
 module.exports = { postNewMssg, getMessages, getMessageById };
