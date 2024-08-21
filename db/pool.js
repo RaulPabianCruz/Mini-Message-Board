8d6c0857914db11447fb2af804de9957c5343735
RaulPabianCruz <ito.pabian@gmail.com>
2024-08-20 18:01:43 -0700

    Update pool.js connectionString depending on env

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,11 @@
 const { Pool } = require('pg')
 
+let connectionString;
+if(process.env.NODE_ENV === 'prod')
+    connectionString = process.env.DATABASE_URL;
+else
+    connectionString = process.env.CONNECTION_STRING;
+
 module.exports = new Pool({
-    connectionString: process.env.CONNECTION_STRING
+    connectionString: connectionString
 });
