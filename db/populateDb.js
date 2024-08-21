f67d7b642c02ddb5b64813f9e00a700a13b865f0
RaulPabianCruz <ito.pabian@gmail.com>
2024-08-21 14:57:36 -0700

    Add a script and file to create the db table

---
+++ db/populateDb.js
@@ -0,0 +1,35 @@
+const { Client } = require('pg');
+
+const SQL = `
+    CREATE TABLE IF NOT EXISTS message_db (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    msg VARCHAR(150),
+    name VARCHAR(25),
+    dateAdded DATE
+    );
+`;
+
+async function main() {
+    console.log('...seeding');
+    let connectionString;
+    if(process.env.NODE_ENV === 'prod')
+        connectionString = process.env.DATABASE_URL;
+    else
+        connectionString = process.env.CONNECTION_STRING;
+
+    const client = new Client({ connectionString: connectionString });
+    
+    try{
+        await client.connect();
+        await client.query(SQL);
+    }
+    catch(e){
+        console.error(e);
+    }
+    finally{
+        client.end();
+        console.log('done');
+    }
+}
+
+main();
